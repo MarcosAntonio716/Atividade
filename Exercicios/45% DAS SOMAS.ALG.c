2bd96ed8955807d25181c060209e657b563cf030
MarcosAntonio716 <marcosesteves2005@gmail.com>
2024-08-21 22:19:46 -0300

    Update and rename 45% DAS SOMAS.ALG to 45% DAS SOMAS.ALG.c

--- Exercicios/45% DAS SOMAS.ALG.c
+++ Exercicios/45% DAS SOMAS.ALG.c
@@ -9,7 +9,7 @@ Inicio
 escreva("Digite 2 numeros para obter 60% da some deles")
 leia(a, c)
 b <- (a+c)*0.60
-escreva("60% da soma dos seus numeros é ",b," ")
+escreva("60% da soma dos seus numeros Ã© ",b," ")
 
 
 
